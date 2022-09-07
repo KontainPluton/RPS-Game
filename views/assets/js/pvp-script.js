359d620ac333e8f0b76b24f8e7cfbd710bd0f25c
Kontain <quentin.bialota@geomatys.com>
2022-09-07 17:27:01 +0200

    feature/pvp-page : Add images and working game

--- views/assets/js/pvp-script.js
+++ views/assets/js/pvp-script.js
@@ -4,7 +4,44 @@ let selectsObjects = document.getElementsByClassName('object-choice')
 let player2Choice = document.getElementById('player2-choice');
 let player1Choice = document.getElementById('player1-choice');
 
+let winnerText = document.getElementById('winnerText');
+let resultText = document.getElementById('resultText');
+
 initPage();
+
+function onSend()
+{
+    let url = "/api/game?object_one=" + player1Choice.value + "&object_two=" + player2Choice.value;
+
+    if(player1Choice.value != '' && player2Choice.value != ''){
+
+        fetch(url)
+        .then(function(response){
+            if(response.status !== 200){
+                throw Error(response.statusText);
+            } else return response.json();
+        })
+        .then(function(data){
+            let winner = data.winner;
+            let outcome = data.outcome;
+            let loser = data.loser;
+    
+            resultText.innerHTML = winner + ' ' + outcome + ' ' + loser;
+    
+            if(winner == player1Choice.value){
+                winnerText.innerHTML = 'Joueur 1 a gagné !';
+            } 
+            else if(loser == player1Choice.value){
+                winnerText.innerHTML = 'Joueur 2 a gagné !';
+            }
+            else{
+                winnerText.innerHTML = 'Egalité !';
+            }
+        })
+        .catch(error => console.log(error));
+
+    }
+}
 
 function initPage(){
 
@@ -30,12 +67,30 @@ function generateBloc(object) {
     let divObject = document.createElement('div');
     divObject.className = "col";
 
+    let ext = ".png";
+
+    if(!checkFileExist("assets/images/"+object+".png")) {
+        ext = ".gif";
+    }
+
     divObject.innerHTML =  '<div class="card"><div class="card-body text-center" style="margin: 1px;padding: 10px;">'
-    + '<img src="assets/img/' + object + '.png" style="width: 53px;">'
-    + '<input type="checkbox" id="' + object + '" + onchange="eventCheckbox(this)">'
+    + '<input type="checkbox" id="' + object + '" + onchange="eventCheckbox(this)"></br>'
+    + '<img src="assets/images/' + object + ext + '" style="width: 53px;">'
     + '<h4 class="card-title">&nbsp;' + object + '</h4></div></div>';
 
     return divObject;
+}
+
+function checkFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+     
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function eventCheckbox(checkboxElem) {
