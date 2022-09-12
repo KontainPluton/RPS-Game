ceac641ec4f5661bbdce5ade1fe1a2e74c0d59fd
Tom L'Hermenier <tomlhermenier@gmail.com>
2022-09-12 16:57:31 +0200

    add swagger to doc (not functionning yet)

--- docs/api.md
+++ docs/api.md
@@ -1,3 +1,5 @@
 # API
 
 Vous trouverez sur cette page la liste des routes API présentes dans RPS-Game.
+
+<swagger-ui src="http://localhost:3000/routes/swagger.json"/>
--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,12 @@ const gameRoute = require('./game')
 
 const router = express.Router();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 router.use('/game', gameRoute);
 router.use('/objects', objectRoute);
 
