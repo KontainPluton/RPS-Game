02b80d1b8684576713f7616ad8b3864941003701
Tom L'Hermenier <tomlhermenier@gmail.com>
2022-09-13 15:10:35 +0200

    try fix

--- test/unit/game.unit.test.js
+++ test/unit/game.unit.test.js
@@ -18,18 +18,12 @@ dotenv.config();
 
 let server = null;
 
-
-
 beforeAll(() => {
     server = app.listen(3000);
 });
-afterEach(async () => {
-    await timeout(500);
-})
-
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+afterAll(() => {
+    server.close();
+});
 
 let object = ["Air","Airplane","Alien","Axe","Baby","Beer","Bicycle","Bird","Blood","Book","Bowl","Brain","Butter","Cage","Camera","Car","Castle","Cat","Chain","Chainsaw","Church","Cloud","Cockroach","Community","Computer","Cross","Cup","Death","Devil","Diamond","Dragon","Duck","Dynamite","Electricity","Fence","Film","Fire","Fish","Gold","Grass","Guitar","Gun","Heart","Helicopter","Home","King","Laser","Law","Lightning","Man","Math","Medusa","Nuke","Platimum","Poison","Police","Porcupine","Power","Prayer","Prince","Princess","Queen","Quicksand","Rain","Rainbow","Robot","Rock","Satan","School","Scissors","Sky","Snake","Spider","Sponge","Sun","Sword","T.V.","Tank","Toilet","Tornado","Train","Tree","Turnip","U.F.O.","Vampire","Video Game","Vulture","Wall","Water","Whip","Wolf","Woman"]
 
